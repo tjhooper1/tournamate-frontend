e2b593efaf1d09d71573eb6a6d9a2fe37f250d18
tjhooper1 <tomh386@gmail.com>
2023-05-14 22:10:44 -0400

    added svg

--- src/components/match/MatchComponent.tsx
+++ src/components/match/MatchComponent.tsx
@@ -11,6 +11,13 @@ const MatchComponent: React.FC<{ match: Match }> = ({
   <div className="match">
     <TeamComponent team={match.team1} />
     <TeamComponent team={match.team2} />
+    <svg
+      className="match-line"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="none"
+    >
+      <line x1="0" y1="50" x2="100" y2="50" stroke="#fff" strokeWidth="2" />
+    </svg>
   </div>
 );
 
